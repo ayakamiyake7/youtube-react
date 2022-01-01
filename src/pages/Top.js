516f5dd975b83972b0f00bdf356257f9426b4c3b
Ayaka Miyake <ayakamiyake7@gmail.com>
2022-01-01 19:31:23 +0000

    Add files.

--- src/pages/Top.js
+++ src/pages/Top.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { fetchPopularData } from "../apis/index";
 import Layout from "../components/layout/Layout";
+import { Store } from "../store/index";
 
 const Top = () => {
-  return <Layout>Top page</Layout>;
+  const { globalState, setGlobalState } = useContext(Store);
+
+  useEffect(() => {
+    fetchPopularData().then((res) => {
+      console.log("data", res);
+      setGlobalState({
+        type: "SET_POPULAR",
+        payload: { popular: res.data.items },
+      });
+    });
+  }, []);
+  return <Layout>TOp</Layout>;
 };
 
 export default Top;
