508de29ba019d236f773086122125bc222cbc96d
Ayaka Miyake <ayakamiyake7@gmail.com>
2022-01-02 11:28:10 +0000

    Add lists.

--- src/pages/Top.js
+++ src/pages/Top.js
@@ -1,7 +1,9 @@
 import React, { useContext, useEffect } from "react";
 import { fetchPopularData } from "../apis/index";
 import Layout from "../components/layout/Layout";
+import VideoGrid from "../components/VideoGrid/VideoGrid";
 import { Store } from "../store/index";
+import VideoGridItem from "../VideoGridItem/VideoGridItem";
 
 const Top = () => {
   const { globalState, setGlobalState } = useContext(Store);
@@ -15,7 +17,23 @@ const Top = () => {
       });
     });
   }, []);
-  return <Layout>TOp</Layout>;
+  return (
+    <Layout>
+      <VideoGrid>
+        {globalState.popular &&
+          globalState.popular.map((popular) => {
+            return (
+              <VideoGridItem
+                id={popular.id}
+                key={popular.id}
+                src={popular.snippet.thumbnails.high.url}
+                title={popular.snippet.title}
+              />
+            );
+          })}
+      </VideoGrid>
+    </Layout>
+  );
 };
 
 export default Top;
